6aea6958f75740ebfce5cae249ca48fc04812be3
NurHadiImamuddin18 <nurhadiimamuddin@gmail.com>
2025-09-19 15:59:41 +0700

    bismillah

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ import gspread
 from google.oauth2.service_account import Credentials
 # setup logging
 import logging
-import json
-from google.oauth2.service_account import Credentials
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -20,10 +18,7 @@ SHEET_JSON = os.getenv("GOOGLE_SHEET_JSON", "long-province-472605-s3-f836d497df0
 SHEET_NAME = os.getenv("SHEET_NAME", "telkmklj")
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
-google_creds = json.loads(os.getenv("GOOGLE_CREDS"))
-google_creds["private_key"] = google_creds["private_key"].replace("\\n", "\n")
-
-creds = Credentials.from_service_account_info(google_creds, scopes=SCOPES)
+creds = Credentials.from_service_account_file(SHEET_JSON, scopes=SCOPES)
 gc = gspread.authorize(creds)
 
 try:
